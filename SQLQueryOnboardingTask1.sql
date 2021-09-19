c05b4dc86e9a77f36b2716e957946028647c349d
Vicky Qi <qiqi509@hotmail.com>
2021-09-18 18:22:17 -0700

    Update SQLQueryOnboardingTask1.sql

--- SQLQueryOnboardingTask1.sql
+++ SQLQueryOnboardingTask1.sql
@@ -2,9 +2,9 @@
 
 --Onboarding Task 1
 
-SELECT p.Id, p.name, pf.CurrentHomeValue, rp.Amount, rp.FrequencyType, tf.Name, pf.yield AS Yield, tp.StartDate, tp.EndDate,
+SELECT p.Id, p.name, pf.CurrentHomeValue, pf.yield AS Yield, tp.StartDate, tp.EndDate,
 
-(CASE
+SUM(CASE
 	WHEN rp.FrequencyType = 3 THEN rp.Amount * (
 		DATEDIFF(MONTH, tp.StartDate, tp.EndDate) -
 		(DATEPART(dd,tp.StartDate) - 1) / DATEDIFF(DAY, tp.StartDate, DATEADD(MONTH, 1, tp.StartDate)) +
@@ -16,7 +16,7 @@ END) AS [TotalPayment]
 , COUNT(js.Status) AS JobAvailable, per.FirstName, per.LastName
 
 FROM [dbo].[Property] p
-JOIN OwnerProperty op ON p.id = op.PropertyId
+JOIN OwnerProperty op ON p.id = op.PropertyId and op.OwnerId = 1426
 JOIN PropertyFinance pf ON op.PropertyId = pf.PropertyId
 JOIN PropertyRentalPayment rp ON p.Id = rp.PropertyId
 JOIN TenantProperty tp ON p.Id = tp.PropertyId
@@ -25,8 +25,8 @@ JOIN Person per ON per.Id = op.OwnerId
 LEFT JOIN job j ON p.id = j.PropertyId AND (j.JobStartDate > tp.StartDate AND j.JobEndDate < tp.EndDate)
 LEFT JOIN JobStatus js ON j.JobStatusId = js.id AND js.Status = 'Open'
 
-where op.OwnerId = 1426 and p.IsActive=1 
-group by js.Status, p.Id, p.name, pf.CurrentHomeValue, rp.Amount, rp.FrequencyType, tf.Name, pf.Yield, tp.StartDate, tp.EndDate, per.FirstName, per.LastName
+where p.IsActive=1
+group by p.Id, p.name, pf.CurrentHomeValue, pf.Yield, tp.StartDate, tp.EndDate, per.FirstName, per.LastName
 
 
 
