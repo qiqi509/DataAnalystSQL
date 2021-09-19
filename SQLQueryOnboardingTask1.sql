a427a89042f36751c90339e3da8a704f5d1f354d
Vicky Qi <qiqi509@hotmail.com>
2021-09-18 18:38:59 -0700

    updated format

--- SQLQueryOnboardingTask1.sql
+++ SQLQueryOnboardingTask1.sql
@@ -2,8 +2,7 @@
 
 --Onboarding Task 1
 
-SELECT p.Id, p.name, pf.CurrentHomeValue, pf.yield AS Yield, tp.StartDate, tp.EndDate,
-
+SELECT p.Id, p.name, pf.CurrentHomeValue, pf.yield AS Yield,
 SUM(CASE
 	WHEN rp.FrequencyType = 3 THEN rp.Amount * (
 		DATEDIFF(MONTH, tp.StartDate, tp.EndDate) -
@@ -12,8 +11,7 @@ SUM(CASE
 	WHEN rp.FrequencyType = 1 THEN rp.Amount * DATEDIFF(week, tp.StartDate, tp.EndDate) 
 	WHEN rp.FrequencyType = 2 THEN rp.Amount * (DATEDIFF(week, tp.StartDate, tp.EndDate)) / 2
 END) AS [TotalPayment], 
-
-COUNT(js.Status) AS JobAvailable, per.FirstName, per.LastName
+COUNT(js.Status) AS JobAvailable, per.FirstName, per.LastName, tp.StartDate, tp.EndDate
 
 FROM [dbo].[Property] p
 JOIN OwnerProperty op ON p.id = op.PropertyId and op.OwnerId = 1426
@@ -25,8 +23,8 @@ JOIN Person per ON per.Id = op.OwnerId
 LEFT JOIN job j ON p.id = j.PropertyId AND (j.JobStartDate > tp.StartDate AND j.JobEndDate < tp.EndDate)
 LEFT JOIN JobStatus js ON j.JobStatusId = js.id AND js.Status = 'Open'
 
-where p.IsActive=1
-group by p.Id, p.name, pf.CurrentHomeValue, pf.Yield, tp.StartDate, tp.EndDate, per.FirstName, per.LastName
+WHERE p.IsActive=1
+GROUP BY p.Id, p.name, pf.CurrentHomeValue, pf.Yield, tp.StartDate, tp.EndDate, per.FirstName, per.LastName
 
 
 
