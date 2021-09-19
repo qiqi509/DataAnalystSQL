5e2c489e78cc1f07acce7a2e49da1bb032bfae21
Vicky Qi <qiqi509@hotmail.com>
2021-09-18 19:00:33 -0700

    Update SQLQueryOnboardingTask2.sql

--- SQLQueryOnboardingTask2.sql
+++ SQLQueryOnboardingTask2.sql
@@ -1,5 +1,5 @@
 
-SELECT Property.Name, CAST(Property.Bedroom AS varchar(10)) + ' Besdrooms' + ', '  + CAST(property.Bathroom AS varchar (10)) + ' Bathrooms'  AS Propertydetails,
+SELECT Property.Name, CAST(Property.Bedroom AS varchar(10)) + ' Bedrooms' + ', '  + CAST(property.Bathroom AS varchar (10)) + ' Bathrooms'  AS Propertydetails,
 pr.Amount AS Rentalpayment, ad.Number + ' ' + ad.Street AS Propertyaddress, pe.Amount AS Expense, CAST(pe.Date as date) as Date, p.FirstName AS CurrentOwner
 
 FROM [dbo].[Property]
@@ -9,4 +9,4 @@ JOIN OwnerProperty op ON Property.Id = op.PropertyId
 JOIN Person p ON p.Id = op.OwnerId
 JOIN Address ad ON Property.AddressId = ad.AddressId
 
-WHERE Property.Name = 'property A' 
+WHERE Property.Name = 'property A' and Property.IsActive = 1
