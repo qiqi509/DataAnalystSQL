dc9ffe5b9cc7620398c9c0c6a2b298e4f885e024
Vicky Qi <qiqi509@hotmail.com>
2021-09-20 01:08:13 -0700

    Update SQLQueryOnboardingTask2.sql

--- SQLQueryOnboardingTask2.sql
+++ SQLQueryOnboardingTask2.sql
@@ -1,12 +1,15 @@
 
-SELECT DISTINCT Property.Name, CONCAT(Property.Bedroom, ' Bedrooms, ', property.Bathroom, ' Bathrooms') AS Propertydetails,
-pr.Amount AS Rentalpayment, ad.Number + ' ' + ad.Street AS Propertyaddress, pe.Amount AS Expense, FORMAT(pe.Date, 'dd MM yyyy') as Date, p.FirstName AS CurrentOwner
+SELECT DISTINCT Property.Name, 
+	CONCAT(Property.Bedroom, ' Bedrooms, ', property.Bathroom, ' Bathrooms') AS PropertyDetails,
+	CONCAT(ad.Number, ' ', ad.Street) AS PropertyAddress, 
+	pr.Amount AS RentalPayment, pe.Amount AS Expense, 
+	FORMAT(pe.Date, 'dd MMM yyyy') as Date, p.FirstName AS CurrentOwner
 
 FROM [dbo].[Property]
-JOIN PropertyRentalPayment pr ON Property.Id = pr.PropertyId
-JOIN PropertyExpense pe ON Property.Id = pe.PropertyId
-JOIN OwnerProperty op ON Property.Id = op.PropertyId
-JOIN Person p ON p.Id = op.OwnerId
-JOIN Address ad ON Property.AddressId = ad.AddressId
+	JOIN PropertyRentalPayment pr ON Property.Id = pr.PropertyId
+	JOIN PropertyExpense pe ON Property.Id = pe.PropertyId
+	JOIN OwnerProperty op ON Property.Id = op.PropertyId
+	JOIN Person p ON p.Id = op.OwnerId
+	JOIN Address ad ON Property.AddressId = ad.AddressId
 
 WHERE Property.Name = 'property A' and Property.IsActive = 1
